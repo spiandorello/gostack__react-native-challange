d00a2a35026406fb8e647ed3bac1b681f273c2f7
Eduardo Da Veiga Spiandorello <eduardo.spiandorello@gmail.com>
2021-01-06 15:26:29 -0300

    Update

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -15,24 +15,22 @@ import formatValue from '../../utils/formatValue';
 
 import { useCart } from '../../hooks/cart';
 
-// Calculo do total
-// Navegação no clique do TouchableHighlight
-
 const FloatingCart: React.FC = () => {
   const { products } = useCart();
 
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((acm, product) => {
+      const productsSubtotal = product.price * product.quantity;
+      return acm + productsSubtotal;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    return products.reduce((acm, product) => acm + product.quantity, 0);
   }, [products]);
 
   return (
